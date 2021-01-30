ba4d82b69ff1dc2e60c158a0215c3c84312c1d8b
Rida Annurfaida <rdnnrfd@gmail.com>
2021-01-30 23:21:47 +0700

    fix

---
+++ components/footer.php
@@ -0,0 +1,9 @@
+<div class="container">
+    <footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+            <div class="copyright text-center my-auto">
+                <span>Copyright &copy; Rida Annurfaida 2021</span>
+            </div>
+        </div>
+    </footer>
+</div>
